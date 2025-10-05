8484849284b5685c68e314005ef6a59d5631d003
Handensaken <maxmax.larsson@gmail.com>
2025-10-05 21:46:13 +0200

    fixed a programming error

--- Assets/Scripts/LightningSpell.cs
+++ Assets/Scripts/LightningSpell.cs
@@ -12,17 +12,17 @@ public class LightningSpell : Spell
 
     public override void CastSpell(Transform origin, Transform target)
     {
+        SpellEffect = ElectricArcEffect;
         base.CastSpell(origin, target);
         for (int i = 1; i < ElectricArcEffect.transform.childCount; i++)
         {
             childObjects[i - 1] = g.transform.GetChild(i);
         }
-        SpellEffect = ElectricArcEffect;
         // g.transform.GetChild(4).transform.position = target.position;
         float f = Vector3.Distance(g.transform.position, target.position) - 3;
 
-     //   var d = childObjects[3].position;
-      //  Vector3.Lerp(origin.position, childObjects[3].position + dir.normalized * f, Time.deltaTime);
+        //   var d = childObjects[3].position;
+        //  Vector3.Lerp(origin.position, childObjects[3].position + dir.normalized * f, Time.deltaTime);
         childObjects[3].position = childObjects[3].position + dir.normalized * f;
 
         childObjects[2].position = childObjects[2].position + dir.normalized * (f * 0.66f) + new Vector3(0, 2, 0);
