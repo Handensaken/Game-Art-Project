4fca4595c7b67ef4bc9bcbdccb38abe24b2d58a2
sinkingravens <77774063+sinkingravens@users.noreply.github.com>
2025-10-07 14:47:17 +0200

    Created a world to get a feel

--- Assets/UI_Folder/uiScript.cs
+++ Assets/UI_Folder/uiScript.cs
@@ -4,15 +4,25 @@ using UnityEngine.InputSystem;
 
 public class uiScript : MonoBehaviour
 {
+	
+	public Image targetImage;
+	public Sprite[] sprites;
+	public int maximumWeaponInt = 5;
+	public int currentWeaponInt = 0;
+	
+	
+	
+	
+	
+	
     public float holdRequirement = 1f;
     public Image fillCircle;
+    public Image checkMark;
 
     private float holdTimer = 0;
     private bool isHolding = false;
-    void onEnable()
-    {
-	    isHolding = false;
-    }
+    private bool isDone = false;
+
 
     // Update is called once per frame
     void Update()
@@ -23,11 +33,50 @@ public class uiScript : MonoBehaviour
 		    fillCircle.fillAmount = holdTimer / holdRequirement;
 		    if(holdTimer >= holdRequirement)
 		    {
-			    
+			    isDone = true;
+			    checkMark.gameObject.SetActive(true);
 		    }
 	    }
     }
 
+    public void rightClick(InputAction.CallbackContext ctx)
+    {
+	    if (ctx.performed)
+	    {
+		    if (currentWeaponInt <= maximumWeaponInt - 1)
+		    {
+			    currentWeaponInt += 1;
+			    targetImage.sprite = sprites[currentWeaponInt];
+			    Debug.Log(currentWeaponInt);
+		    }
+		    else
+		    {
+			    currentWeaponInt = 0;
+			    targetImage.sprite = sprites[currentWeaponInt];
+		    }
+			    
+	    }
+    }
+    public void leftClick(InputAction.CallbackContext ctx)
+    {
+	    if (ctx.performed)
+	    {
+		    if (currentWeaponInt > 0)
+		    {
+			    currentWeaponInt -= 1;
+			    targetImage.sprite = sprites[currentWeaponInt];
+			    Debug.Log(currentWeaponInt);
+			  
+		    }
+		    else
+		    {
+			    currentWeaponInt = maximumWeaponInt;
+			    targetImage.sprite = sprites[currentWeaponInt];
+		    }
+			    
+	    }
+    } 
+    
     public void OnHold(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
@@ -35,7 +84,7 @@ public class uiScript : MonoBehaviour
 	        Debug.Log("cFUCK");
 	        isHolding = true;
 	        //StartHold();
-        }else if (ctx.canceled)
+        }else if (ctx.canceled && !isDone)
 		{
 			Debug.Log("canceled");
 			ResetHold();
